e8c57abd7064544bc509be71632c9ebd15b509db
shean su <shean012s@gmail.com>
2020-03-06 15:48:32 +0800

    update the unit test plugin

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,10 @@
 import * as Koa from 'koa';
 import * as DebugLogger from 'koa-logger';
-import * as JsonParser from 'koa-json';
-// import * as Static from 'koa-static';
-// import * as path from 'path';
+import * as Static from 'koa-static';
+import * as path from 'path';
 import * as compress from 'koa-compress';
 import bodyParser from './middlewares/bodyParser';
 import { onError } from './middlewares/errorCatch';
-// const historyApiFallback = require('koa2-connect-history-api-fallback');
 const cors = require('@koa/cors');
 
 const app = new Koa();
@@ -21,33 +19,19 @@ app.use(compress({
   flush: require('zlib').Z_SYNC_FLUSH
 }));
 
-// app.use(historyApiFallback({
-//   whiteList: [
-//   ]
-// }));
-
-// koa-static
-// app.use(Static(path.join(__dirname, '../public'), {
-//   maxage: process.env.NODE_ENV === 'production'
-//     ? (1000 * 60 * 60)
-//     : 0
-// }));
-// app.use(Static(path.join(__dirname, '../client/dist'), {
-//   maxage: process.env.NODE_ENV === 'production'
-//     ? (1000 * 60 * 60)
-//     : 0
-// }));
+app.use(Static(path.join(__dirname, '../static'), {
+  maxage: process.env.NODE_ENV === 'production'
+    ? (1000 * 60 * 60)
+    : 0
+}));
 
 // corrs
 app.use(cors());
+// catch error
 app.use(onError);
-
 // parser
 app.use(bodyParser);
-app.use(JsonParser());
 
-// app.use(router.routes());
+
 
 app.listen(3000);
-
-console.log("Server running on port 3000");
--- server/middlewares/errorCatch.ts
+++ server/middlewares/errorCatch.ts
@@ -6,6 +6,7 @@ export const onError = async(ctx: IContext, next: any) => {
   try {
     await next();
   } catch (err) {
+    // 开发环境下将错误打印
     if (process.env.NODE_ENV === 'development') console.log(err);
     ctx.status = err.status || 500;
     ctx.body = {
